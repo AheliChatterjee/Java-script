81f53f7e66c57d1c46a73322f77cf8973a55e7d8
Aheli Chatterjee <chatterjeeaheli2005@gmail.com>
2024-10-26 19:40:21 +0000

    variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+ accountId = 2
+// console.log(accountId);
+
+const account = "Aheli's Account"
+// const keyword is for declairing constant
+//console.log(account);
+
+//account = "Not Aheli's Account"  //Not possible as Constant can not be changed
+//console.log(account);
+
+var Name = "Aheli" // keyword for declairing variable {not used for now}
+let point = 40 // keyword for declairing variable 
+
+//console.log([Name,point])  //this is the process of printing multiple value 
+
+point= 30 //we can change the value of variable
+
+//console.log(point);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
