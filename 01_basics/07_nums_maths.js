83ba2adf4ed855893af200d7c5a3c867614cbf3b
Aheli Chatterjee <chatterjeeaheli2005@gmail.com>
2024-12-05 14:16:15 +0000

    nums

--- 01_basics/07_nums_maths.js
+++ 01_basics/07_nums_maths.js
@@ -1,28 +1,28 @@
 const score = 400
-// console.log(score);
+console.log(score);
 
-const balance = new Number(100)
-// console.log(balance);
+const balance = new Number(100)    //An object that represents a number of any kind. All JavaScript numbers are 64-bit floating-point numbers.
+ console.log(balance);
 
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(1));
+ console.log(balance.toString().length);
+console.log(balance.toFixed(4));
 
 const otherNumber = 123.8966
 
-// console.log(otherNumber.toPrecision(4));
+console.log(otherNumber.toPrecision(4));
 
 const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));
+console.log(hundreds.toLocaleString('en-IN'));
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
-// console.log(Math);
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.6));
-// console.log(Math.ceil(4.2));  //always takes the higher value for a decimal ---> 4.2 will be 5
-// console.log(Math.floor(4.9)); //always takes the lower value for a decimal ---> 4.9 will be 4
-// console.log(Math.min(4, 3, 6, 8));
-// console.log(Math.max(4, 3, 6, 8));
+ console.log(Math);
+ console.log(Math.abs(-4));
+ console.log(Math.round(4.6));
+ console.log(Math.ceil(4.2));  //always takes the higher value for a decimal ---> 4.2 will be 5
+ console.log(Math.floor(4.9)); //always takes the lower value for a decimal ---> 4.9 will be 4
+ console.log(Math.min(4, 3, 6, 8));
+ console.log(Math.max(4, 3, 6, 8));
 
 console.log(Math.random());
 console.log((Math.random()*10) + 1);
