948f69b0696d7ddb59216191d49f98a70c8f37db
Aheli Chatterjee <chatterjeeaheli2005@gmail.com>
2024-12-08 05:53:28 +0000

    array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -4,26 +4,25 @@ const myArr = [0, 1, 2, 3, 4, 5]
 const myHeors = ["shaktiman", "naagraj"]
 
 const myArr2 = new Array(1, 2, 3, 4)
-// console.log(myArr[1]);
+console.log(myArr[1]);
 
-// Array methods
+//Array methods
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
 
-// myArr.push(6)
-// myArr.push(7)
-// myArr.pop()
+myArr.unshift(9)
+myArr.shift()
 
-// myArr.unshift(9)
-// myArr.shift()
+//read notes from green diary for more info on this operations
 
-// read notes from green diary for more info on this operations
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(3));
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
+const newArr = myArr.join()
 
-// const newArr = myArr.join()
-
-// console.log(myArr);
-// console.log( newArr);
+console.log(myArr);
+console.log( newArr);
 
 
 // slice, splice
