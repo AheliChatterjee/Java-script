359a201a45b6c7ac493b0160eb8a7b6ffd710e05
Aheli Chatterjee <chatterjeeaheli2005@gmail.com>
2024-12-10 03:52:09 +0000

    scopes

--- 03_basics/04_IIFE.js
+++ 03_basics/04_IIFE.js
@@ -6,6 +6,9 @@
     console.log(`DB CONNECTED`);
 })();
 
+// to secure the function from the pollution of global scope we use iife
+//iife ->>which execute immidietly
+
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
 } )('hitesh')
